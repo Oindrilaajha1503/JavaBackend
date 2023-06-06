d9a605507bbf7b4cc5f3a16f55e34abf1ec893fd
Oindrila Ajha <125449379+Oindrilaajha1503@users.noreply.github.com>
2023-06-06 12:56:36 +0530

    Add files via upload

---
+++ h2h_milestone_1/src/com/highradius/model/Invoice.java
@@ -0,0 +1,19 @@
+package com.highradius.model;
+
+public class Invoice {
+	private int id;
+	public Invoice(int id) {
+		super();
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
+
+
+	
+
